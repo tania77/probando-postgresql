9d24702863d67b7b0ccbec34eba7680cb3d5ca4e
tania77 <ainatrg@gmail.com>
2016-12-02 10:24:20 +0000

    Actulizando estructura

--- probando.js
+++
@@ -1,8 +0,0 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'https://sytw-tania77.c9users.io';
-
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-  'CREATE TABLE pac(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
